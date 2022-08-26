7a97c593dbcedba077f16d94ffae8aae6ad69b25
jaroslav <jaroslav.stedronsky@cgi.com>
2022-08-26 14:24:07 +0200

    progrma 2 zahrnuje funkce 3 a 4.

--- program2.c
+++ program2.c
@@ -12,3 +12,12 @@ int funkce4(int a)
 printf("jsem funkce 4 %d konec.", a);
 wait(1000);
 }
+
+
+int funkce3(int param)
+# tady bude tělo funkce
+{
+printf("jsem funkce 3, mám parametr:  %d , konec.", param);
+
+wait(1000);
+}
